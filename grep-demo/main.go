30078776035651cea7bf8fbb955ac96fdf829248
hemanth <hemanthjagaari@gmail.com>
2023-01-03 10:29:18 +0530

    modulesetup

--- grep-demo/main.go
+++ grep-demo/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	args := os.Args
 	fmt.Println("In main function")
+	fmt.Printf("Type of cmd args is %T\n", args)
+	fmt.Printf("cmd line arguments are %v\n", args)
+
+	wdir, err := os.Getwd()
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("working dir :%v\n", wdir)
 }
